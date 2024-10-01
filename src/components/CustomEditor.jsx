b009fed8d18907bd15ad88a81ce5d05b6ea94c71
swansea <leesuwan89@gmail.com>
2024-10-02 05:20:28 +0900

    이미지 띄우기

--- src/components/CustomEditor.jsx
+++ src/components/CustomEditor.jsx
@@ -1,5 +1,6 @@
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
+import axios from "axios";
 import { useRef } from "react";
 
 const CustomEditor = () => {
@@ -8,6 +9,27 @@ const CustomEditor = () => {
   const onChange = () => {
     const data = editorRef.current.getInstance().getHTML();
     console.log(data);
+  };
+
+  const onUploadImage = async (blob, cb) => {
+    const formData = new FormData();
+    formData.append("files", blob);
+
+    const uploadResponse = await axios.post(
+      "http://localhost:8080/board/images",
+      formData
+    );
+    const filename = uploadResponse.data;
+
+    /* const getResponse = await axios.get("http://localhost:8080/board/images", {
+      params: { filename: encodeURI(filename) },
+    });
+ */
+    const imageUrl = `http://localhost:8080/board/images?filename=${encodeURI(
+      filename
+    )}`;
+    console.log(imageUrl);
+    cb(imageUrl);
   };
 
   return (
@@ -22,6 +44,9 @@ const CustomEditor = () => {
         initialEditType="wysiwyg"
         useCommandShortcut={false}
         language="ko-KR"
+        hooks={{
+          addImageBlobHook: onUploadImage,
+        }}
       />
     </div>
   );
